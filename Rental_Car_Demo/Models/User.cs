5946670381dee06eb3ec047db0e3b525cb5e2f79
tuankietnvu <kietnvthe179021@fpt.edu.vn>
2024-07-01 15:20:41 +0700

    merge register vao login

--- Rental_Car_Demo/Models/User.cs
+++ Rental_Car_Demo/Models/User.cs
@@ -11,7 +11,7 @@ public partial class User
 
     public string Password { get; set; } = null!;
 
-    public bool? RememberMe { get; set; }
+    public bool RememberMe { get; set; }
 
     public bool? Role { get; set; }
 
--- Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6cb1bae85cd101f35ec55476bfc8980b1c6a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
