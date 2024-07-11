c8e4c7b88046ac8086327735ac3b628ab5c56429
tuankietnvu <kietnvthe179021@fpt.edu.vn>
2024-07-11 10:40:48 +0700

    done

--- Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496a18bc5575cc3d1206fb829667a9ab0ae89890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80bb8a89d337169638f8202080fb0b2a60646b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
