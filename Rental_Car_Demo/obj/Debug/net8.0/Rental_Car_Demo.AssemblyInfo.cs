97c4562926d3be27bf8ec7c15bb5da1d816ffe6e
bachnhhe173308 <bachnhhe173308@fpt.edu.vn>
2024-07-11 15:14:21 +0700

    fix css carousel

--- Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0999baa6714e9227b398a867b8511aa25eefcdb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69450d7ed1f299550066feb69aead0d997c95084")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
