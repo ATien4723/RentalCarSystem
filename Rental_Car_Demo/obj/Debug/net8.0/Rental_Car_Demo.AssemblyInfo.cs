b7f74bac233cdd8c42dfad6fe9913a151b27c929
Truong An Vu <truonganat0@gmail.com>
2024-07-18 13:26:55 +0700

    update edit booking part1

--- Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c95b6eb4cfdcb35ba4ac498bf4d9809a10781e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a064b6bf819502771aa036d79a604dee7548298")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
