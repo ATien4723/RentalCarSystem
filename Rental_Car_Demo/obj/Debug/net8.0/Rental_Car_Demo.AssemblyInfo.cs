2ed7b98cdbf8e7377fcf96022cb2a1ddcf9d3b5c
Truong An Vu <truonganat0@gmail.com>
2024-07-23 14:01:46 +0700

    update tiep

--- Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Debug/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094fbf54851be6182bc17a25c6e58948ed139f38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd167cc023d9559009b39f8ecb2c3d31444ca1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
