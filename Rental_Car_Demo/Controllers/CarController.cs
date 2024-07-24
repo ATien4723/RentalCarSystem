33d2df51c31b7741317cba331abcb1ae25a309cd
CDQuan88 <duyquan7b@gmail.com>
2024-07-24 15:41:34 +0700

    update UI ViewCarDetails

--- Rental_Car_Demo/Controllers/CarController.cs
+++ Rental_Car_Demo/Controllers/CarController.cs
@@ -20,6 +20,97 @@ namespace Rental_Car_Demo.Controllers
             _emailService = emailService;
         }
 
+        public IActionResult tet1(int CarId, string? location, DateTime? startDate, DateTime? endDate)
+        {
+            ViewBag.location = location;
+            ViewBag.startDate = startDate;
+            ViewBag.endDate = endDate;
+
+            var car = _db.Cars.FirstOrDefault(x => x.CarId == CarId);
+            ViewBag.CarOwner = _db.Users.FirstOrDefault(x => x.UserId == car.UserId);
+            var userJson = HttpContext.Session.GetString("User");
+            bool checkRent = false;
+            if (!string.IsNullOrEmpty(userJson))
+            {
+                var user = JsonConvert.DeserializeObject<User>(userJson);
+                List<Booking> lBook = _db.Bookings.Where(x => x.CarId == CarId && x.UserId == user.UserId).ToList();
+                foreach (Booking booking in lBook)
+                {
+                    if (booking.Status == 3 || booking.Status == 4)
+                    {
+                        checkRent = true;
+                        break;
+                    }
+                }
+
+            }
+            var matchedFeedback = (from feedback in _db.Feedbacks
+                                   join booking in _db.Bookings on feedback.BookingNo equals booking.BookingNo
+                                   join user in _db.Users on booking.UserId equals user.UserId
+                                   where booking.CarId == CarId && feedback.Ratings > 0
+                                   select new
+                                   {
+                                       feedback.FeedbackId,
+                                       feedback.BookingNo,
+                                       feedback.Ratings,
+                                       feedback.Content,
+                                       feedback.Date,
+                                       user.Name,
+                                   }).ToList();
+
+            ViewBag.matchedFeedback = matchedFeedback.OrderByDescending(x => x.Date).ToList();
+
+
+
+            double rating = 0, nor = 0;
+            foreach (var o in matchedFeedback)
+            {
+                if (o.Ratings < 0)
+                {
+                    continue;
+                }
+                rating += o.Ratings;
+                nor += 1;
+            }
+
+
+
+            if (nor > 0)
+            {
+                rating = rating / nor;
+                rating = (Math.Ceiling(rating * 2)) / 2.0;
+            }
+            else
+            {
+                rating = 0;
+            }
+
+            ViewBag.matchedFeedback = matchedFeedback.OrderByDescending(x=> x.Date);
+            ViewBag.Rating = rating;
+            var brand = _db.CarBrands.FirstOrDefault(x => x.BrandId == car.BrandId);
+            var model = _db.CarModels.FirstOrDefault(x => x.ModelId == car.ModelId);
+            var document = _db.CarDocuments.FirstOrDefault(x => x.DocumentId == car.DocumentId);
+            var color = _db.CarColors.FirstOrDefault(x => x.ColorId == car.ColorId);
+            var address = _db.Addresses.FirstOrDefault(x => x.AddressId == car.AddressId);
+            var ward = _db.Wards.FirstOrDefault(x => x.WardId == address.WardId);
+            var district = _db.Districts.FirstOrDefault(x => x.DistrictId == address.DistrictId);
+            var city = _db.Cities.FirstOrDefault(x => x.CityId == address.CityId);
+            var term = _db.TermOfUses.FirstOrDefault(x => x.TermId == car.TermId);
+            var function = _db.AdditionalFunctions.FirstOrDefault(x => x.FucntionId == car.FucntionId);
+            ViewBag.car = car;
+            ViewBag.brand = brand;
+            ViewBag.model = model;
+            ViewBag.document = document;
+            ViewBag.color = color;
+            ViewBag.address = address;
+            ViewBag.ward = ward;
+            ViewBag.district = district;
+            ViewBag.city = city;
+            ViewBag.term = term;
+            ViewBag.function = function;
+            ViewBag.checkRent = checkRent;
+            return View();
+        }
         public ActionResult AddACar()
         {
             var context = new RentCarDbContext();
