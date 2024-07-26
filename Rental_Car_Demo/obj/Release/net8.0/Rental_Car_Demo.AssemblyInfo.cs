cbda4a544ba684e288c668ab359ef9914fe4f7c2
CDQuan88 <duyquan7b@gmail.com>
2024-07-26 13:08:16 +0700

    deploy

--- Rental_Car_Demo/obj/Release/net8.0/Rental_Car_Demo.AssemblyInfo.cs
+++ Rental_Car_Demo/obj/Release/net8.0/Rental_Car_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2a6b58d5e0068ce9cebf159aa7ab733c6884c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1833e0e08d349d00f54f910c9c16b32c42b6140")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rental_Car_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
