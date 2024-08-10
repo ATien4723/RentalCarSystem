100833933a2aa05a467690aae02e33592e73843c
tuankietnvu <kietnvthe179021@fpt.edu.vn>
2024-08-10 21:24:32 +0700

    abcssss

--- Rental_Car_DemoTests/HomeControllerTests.cs
+++ Rental_Car_DemoTests/HomeControllerTests.cs
@@ -507,45 +507,4 @@ namespace Rental_Car_Demo.Tests
         }
     }
 
-
-
-    [ExcludeFromCodeCoverage]
-    public class DummySession : ISession
-    {
-        private readonly Dictionary<string, byte[]> _sessionStorage = new Dictionary<string, byte[]>();
-
-        public bool IsAvailable => true;
-        public string Id => Guid.NewGuid().ToString();
-        public IEnumerable<string> Keys => _sessionStorage.Keys;
-
-        public void Clear()
-        {
-            _sessionStorage.Clear();
-        }
-
-        public Task CommitAsync(CancellationToken cancellationToken = default)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task LoadAsync(CancellationToken cancellationToken = default)
-        {
-            return Task.CompletedTask;
-        }
-
-        public void Remove(string key)
-        {
-            _sessionStorage.Remove(key);
-        }
-
-        public void Set(string key, byte[] value)
-        {
-            _sessionStorage[key] = value;
-        }
-
-        public bool TryGetValue(string key, out byte[] value)
-        {
-            return _sessionStorage.TryGetValue(key, out value);
-        }
-    }
 }
